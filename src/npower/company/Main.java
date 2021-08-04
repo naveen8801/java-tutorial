1b8a6bed553fc6fb30e530afaf00686016b31c80
naveen8801 <naveensharma10d@gmail.com>
2021-08-04 12:09:21 +0530

    Input Output tutorial & Primitive datatype tutorial added

---
+++ src/npower/company/Main.java
@@ -0,0 +1,16 @@
+package npower.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Output
+        System.out.println("Hello World");
+
+        //Input
+        Scanner name = new Scanner(System.in);
+        System.out.println(name.nextLine());
+    }
+}
+
---
+++ src/npower/company/Primitives.java
@@ -0,0 +1,16 @@
+package npower.company;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        // Primitive datatype are those which can not be break into pieces
+        int age = 20;
+        char letter = 'n';
+        float marks = 85.4f;
+        double large = 545354.34534;
+        long largeIntValues = 222443453435335345L;
+        boolean checked = true;
+
+    }
+}
+
